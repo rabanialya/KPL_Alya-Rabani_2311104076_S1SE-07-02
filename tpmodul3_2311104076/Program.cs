dfac33a0607e1619df41cc06f4fa8ea2c51fe31d
alya <alyarabani33@gmail.com>
2025-03-04 01:34:20 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_2311104076/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tpmodul3_2311104076;
+
+class Program
+{
+    static void Main()
+    {
+        KodePos kodePos = new KodePos();
+
+        Console.Write("Masukkan nama kelurahan: ");
+        string kelurahan = Console.ReadLine();
+
+        string hasilKodePos = kodePos.GetKodePos(kelurahan);
+        Console.WriteLine($"Kode pos {kelurahan}: {hasilKodePos}");
+    }
+}
